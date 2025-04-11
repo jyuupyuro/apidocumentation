c7d13f545385a0c816816cacf27b9aa7a3b97cad
Vincent Lam <lamweishen54@gmail.com>
2025-04-11 18:44:09 +0800

    set up enviroment

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <p>Testing a Font</p>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -26,5 +27,8 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+p {
+  font-family: 'PT Serif', serif;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import './style.css'
-import App from './App.vue'
+// Import Bootstrap and BootstrapVue 3 CSS files
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import BootstrapVue3 from 'bootstrap-vue-3';
+// Import fontawesome Icons
+import './assets/fontawesome/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+// Import the PT Serif font definitions
+import './assets/fonts/PT_Serif.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(BootstrapVue3);
+
+app.mount('#app');
