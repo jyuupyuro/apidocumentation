79deb39feaed848c8ca4660f255d95307d7a8f45
jyuupuro <weishen.lam@hippo-tech.com>
2025-04-16 17:34:08 +0800

    enchance forminput

--- src/components/common/input.vue
+++ src/components/common/input.vue
@@ -1,27 +1,37 @@
 <template>
     <div class="input-wrapper">
-        <!-- Optional icon on the left -->
-        <div class="input-icon ">
+        <!-- Optional icon -->
+        <div class="input-icon">
             <slot name="icon" />
         </div>
-        <!-- Input field -->
-        <b-form-input :value="modelValue" @input="$emit('update:modelValue', $event)" :type="type"
+
+        <!-- Select Input -->
+        <b-form-select v-if="type == 'select'" :value="modelValue" @change="$emit('update:modelValue', $event)"
+            :options="options" :class="['custom-input', inputClass]" />
+
+        <!-- Input -->
+        <b-form-input v-else :value="modelValue" @input="$emit('update:modelValue', $event)"
             :placeholder="placeholder" :class="['custom-input', inputClass]" />
     </div>
 </template>
 
+
 <script setup>
 defineProps({
-  modelValue: String,
-  type: {
-    type: String,
-    default: 'text'
-  },
-  placeholder: String,
-  inputClass: {
-    type: [String, Array, Object],
-    default: ''
-  }
+    modelValue: [String, Number],
+    type: {
+        type: String,
+        default: 'text' // can also be 'select'
+    },
+    placeholder: String,
+    options: {
+        type: Array,
+        default: () => []
+    },
+    inputClass: {
+        type: [String, Array, Object],
+        default: ''
+    }
 });
 
 defineEmits(['update:modelValue']);
--- src/views/ui-kit.vue
+++ src/views/ui-kit.vue
@@ -3,6 +3,15 @@
         <h2 class="mb-4">UI Components Showcase</h2>
 
         <section class="mb-5 ui">
+            <h5>Typography</h5>
+            <p class="display-6">Display Heading</p>
+            <p class="h1">Heading 1</p>
+            <p class="h2">Heading 2</p>
+            <p class="lead">This is lead text</p>
+            <p>This is normal paragraph text.</p>
+        </section>
+
+        <section class="mb-5 ui">
             <h5>Buttons</h5>
             <div class="d-flex gap-3 flex-wrap justify-content-center">
                 <Button variant="primary">Primary</Button>
@@ -13,22 +22,19 @@
         </section>
 
         <section class="mb-5 ui">
-            <h5>Typography</h5>
-            <p class="display-6">Display Heading</p>
-            <p class="h1">Heading 1</p>
-            <p class="h2">Heading 2</p>
-            <p class="lead">This is lead text</p>
-            <p>This is normal paragraph text.</p>
-        </section>
-
-        <section class="mb-5 ui">
             <h5>Form Elements</h5>
             <div class="d-flex gap-3 flex-wrap justify-content-center">
-                <Input v-model="email" placeholder="Email address">
-                <template #icon>
-                    <i class="fas fa-envelope" data-icon-left></i>
-                </template>
-                </Input>
+                <div>
+                    <Input v-model="email" placeholder="Email address">
+                    <template #icon>
+                        <i class="fas fa-envelope" data-icon-left></i>
+                    </template>
+                    </Input>
+                </div>
+                <div>
+                    <Input v-model="testing" type="select"
+                        :options="[{ value: '', text: 'Select an option' }, { value: '', text: 'Another option' }]" />
+                </div>
             </div>
         </section>
     </div>
